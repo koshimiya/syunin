ce466894e921345a7b873c9d11532fa86503bacf
Suguru FUJISAWA <udumge@gmail.com>
2014-03-29 20:09:34 +0900

    create template

---
+++ FizzBuzz/test/in/plam/ajm/syunin/test/FizzBuzzTest.java
@@ -0,0 +1,53 @@
+package in.plam.ajm.syunin.test;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import in.plam.ajm.syunin.FizzBuzz;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author udumbara
+ */
+public class FizzBuzzTest {
+    
+    public FizzBuzzTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class FizzBuzz.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FizzBuzz.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
